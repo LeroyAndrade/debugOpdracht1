73d3cef61bd668b7e62e19671b12cd95f3eb119d
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2019-12-09 04:34:52 +0100

    Opdracht zonder bonus
    Opdracht zonder bonus

--- stap1/get-started.js
+++ stap1/get-started.js
@@ -5,16 +5,11 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License. */
-
-/*
-Bron origineel document: https://developers.google.com/web/tools/chrome-devtools/javascript/
-*/ 
+limitations under the License. */ /* Bron origineel document: https://developers.google.com/web/tools/chrome-devtools/javascript/ */
 
 function onClick() {
   if (inputsAreEmpty()) {
@@ -24,7 +19,7 @@ function onClick() {
   updateLabel();
 }
 function inputsAreEmpty() {
-  if (getNumber1() === '' || getNumber1() === NULL|| getNumber2() === '' || getNumber2() === NULL) {
+  if (getNumber1() === '' || getNumber2() === '') {
     return true;
   } else {
     return false;
@@ -33,8 +28,9 @@ function inputsAreEmpty() {
 function updateLabel() {
   var addend1 = getNumber1();
   var addend2 = getNumber2();
-  var sum = (addend1 + addend2);
-  label.textContent = (addend1 + addend2) + ' = ' + sum;
+  var sum=(parseInt(addend1) + parseInt(addend2));//var sum = addend1 + addend2;
+  label.textContent = "addend1" + ' + ' + "addend2" + ' = ' + sum;
+  console.log(parseInt(addend1) + parseInt(addend2));
 }
 function getNumber1() {
   return inputs[0].value;
